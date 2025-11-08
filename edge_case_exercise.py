d771ad7abe0a1b771cab7705447e11fd9e4c4f53
322781188 <galnattibr@gmail.com>
2025-11-08 14:05:36 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -19,17 +19,18 @@ def move(my_list, direction=None):
     return my_list
 
 
-n_terms = 1000
-list_of_numbers = []
-for i in range(n_terms):
-    if i % 2 != 0:
-        list_of_numbers.append(-1)
-    else:
-     list_of_numbers.append(1)
-odd = 3 
-for i in range(1, n_terms):
-    list_of_numbers[i] = list_of_numbers[i] / odd
-    odd += 2
-tozaa = 4*sum(list_of_numbers)
+ef pay():
+  n_terms = 10
+  list_of_numbers = []
+  for i in range(n_terms):
+      if i % 2 != 0:
+          list_of_numbers.append(-1)
+      else:
+       list_of_numbers.append(1)
+  odd = 3 
+  for i in range(1, n_terms):
+      list_of_numbers[i] = list_of_numbers[i] / odd
+      odd += 2
+  tozaa = 4*sum(list_of_numbers)
 
-print(tozaa)
+  return tozaa
