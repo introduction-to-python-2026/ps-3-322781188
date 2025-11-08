4b8364216d5f0c376fb2fa5fbe4436bea899724b
322781188 <galnattibr@gmail.com>
2025-11-08 14:28:05 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -17,3 +17,20 @@ def move(my_list, direction=None):
         my_list[index_of_one - 1] = 1
 
     return my_list  
+
+
+def approximate_pi(n_terms):
+  n_terms = 1000
+  list_of_numbers = []
+  for i in range(n_terms):
+      if i % 2 != 0:
+          list_of_numbers.append(-1)
+      else:
+       list_of_numbers.append(1)
+  odd = 3 
+  for i in range(1, n_terms):
+      list_of_numbers[i] = list_of_numbers[i] / odd
+      odd += 2
+  tozaa = 4*sum(list_of_numbers)
+
+  return tozaa
