ecc4c2753ad0bab2b514ab95f62ebffdd5a2ec74
322781188 <galnattibr@gmail.com>
2025-11-08 14:16:31 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -16,24 +16,4 @@ def move(my_list, direction=None):
         my_list[index_of_one] = 0
         my_list[index_of_one - 1] = 1
 
-    return my_list
-    the_pig_location = [0, 0, 0, 1, 0, 0]
-    print(move(the_pig_location, "right"))  
-
-
-
-def pay():
-  n_terms = 10
-  list_of_numbers = []
-  for i in range(n_terms):
-      if i % 2 != 0:
-          list_of_numbers.append(-1)
-      else:
-       list_of_numbers.append(1)
-  odd = 3 
-  for i in range(1, n_terms):
-      list_of_numbers[i] = list_of_numbers[i] / odd
-      odd += 2
-  tozaa = 4*sum(list_of_numbers)
-
-  return tozaa
+    return my_list  
