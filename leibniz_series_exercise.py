8d60f2b458dbb68dad1819a70910a07c0e55915c
322781188 <galnattibr@gmail.com>
2025-11-08 15:05:43 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,5 +1,5 @@
 def approximate_pi(n_terms):
-       list_of_numbers = []
+       list_of_alternating_signs = []
        for i in range(n_terms):
-           list_of_numbers.append((-1)**i/(2*i+1)) 
-       return 4*sum(list_of_numbers)
+           list_of_alternating_signs.append((-1)**i/(2*i+1)) 
+       return 4*sum(list_of_alternating_signs)
