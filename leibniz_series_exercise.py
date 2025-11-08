16318421d942f88490bc0779e8f2fb620ab3d5ed
322781188 <galnattibr@gmail.com>
2025-11-08 14:42:21 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,15 +1,5 @@
-def approximate_pi(n_terms):
-  n_terms = 1000
+dedef approximate_pi(n_terms):
   list_of_numbers = []
   for i in range(n_terms):
-      if i % 2 != 0:
-          list_of_numbers.append(-1)
-      else:
-       list_of_numbers.append(1)
-  odd = 3 
-  for i in range(1, n_terms):
-      list_of_numbers[i] = list_of_numbers[i] / odd
-      odd += 2
-  tozaa = 4*sum(list_of_numbers)
-
-  return tozaa
+      list_of_numbers.append((-1)**1(2*1+1))
+  return 4*sum(list_of_numbers)
